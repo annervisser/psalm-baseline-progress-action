b8545e16ea3c9566c285ff198e200cb7f74aba4c
Anner Visser <a.h.c.visser@scienta.nl>
2023-02-15 14:29:15 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 set -euv
 
-echo current:
-git show "$GITHUB_REF:psalm-baseline.xml" | xmllint --xpath 'count(//file[not(starts-with(@src, "test"))]/*/code)' -
-echo old:
-git show "$GITHUB_BASE_REF:psalm-baseline.xml" | xmllint --xpath 'count(//file[not(starts-with(@src, "test"))]/*/code)' -
+ls -alh
+
+#echo current:
+#git show "$GITHUB_REF:psalm-baseline.xml" | xmllint --xpath 'count(//file[not(starts-with(@src, "test"))]/*/code)' -
+#echo old:
+#git show "$GITHUB_BASE_REF:psalm-baseline.xml" | xmllint --xpath 'count(//file[not(starts-with(@src, "test"))]/*/code)' -
